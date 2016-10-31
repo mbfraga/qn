2b5339277a9057c93993bf4c906bf7b8680fa6f4
Martin B. Fraga <mbfraga@gmail.com>
2016-10-30 17:12:14 -0700

    Separated python code into rofi- and non-rofi-specific functions

--- qn.py
+++ qn.py
@@ -2,13 +2,12 @@ import os
 import sys
 import subprocess
 from subprocess import Popen,PIPE
-import struct
 import magic # to detect mimetypes
 
-import mmap
+#import mmap
 
-import time
-timea = time.time()
+#import time
+#timea = time.time()
 
 #import notify2
 
@@ -19,86 +18,23 @@ timea = time.time()
 
 # User-defined Globals
 
-QNDIR = os.path.join(os.path.expanduser("~"), "syncthing/smalldocs/quicknotes")
-#QNDIR = os.path.join(os.path.expanduser("~"), "qn_test")
-COLS=3
+#QNDIR = os.path.join(os.path.expanduser("~"), "syncthing/smalldocs/quicknotes")
+QNDIR = os.path.join(os.path.expanduser("~"), "qn_test2")
 QNTERMINAL='urxvt'
 #QNBROWSER=chromium # not used
 QNEDITOR='nvim'
 
 
 # Globals
-
-# delete note
-opt_delete = 'Alt+Backspace'
-# see trashed notes with the ability to restore them
-opt_seetrash = 'Alt+t'
-# rename note
-opt_rename = 'Alt+Space'
-# Open selected note's directory (not yet implemented)
-opt_open_dir = 'Alt+d'
-# Grep notes (not yet implemented)
-opt_filter_content = 'Alt+s'
-# Force create a note
-opt_force_new = 'Alt+Return'
-
 QNDATA = os.path.join(QNDIR, '.qn')
 QNTRASH = os.path.join(QNDATA, 'trash')
-rofi_base_command = ['rofi', '-dmenu', '-p', 'qn:']
+TERM_INTER = False
 
-
-# code borrowed from
-# https://github.com/DaveDavenport/Rofication/blob/master/rofication-gui.py
-def call_rofi(rofi_command, entries, additional_args=[]):
-    additional_args.extend([ '-kb-custom-9', opt_delete,
-                             '-kb-custom-8', opt_seetrash,
-                             '-kb-custom-7', opt_rename,
-                             '-kb-custom-6', opt_open_dir,
-                             '-kb-custom-5', opt_filter_content,
-                             '-kb-custom-4', opt_force_new,
-                             '-markup-rows',
-                             '-sep', '\\0',
-#                             '-format', 'i',
-                             '-columns', str(COLS),
-#                             '-lines', '4',
-#                             '-eh', '2',
-#                             '-location', '2', '-width', '100' ])
-                            ])
-
-    proc = Popen(rofi_command + additional_args, stdin=PIPE, stdout=PIPE)
-
-    for e in entries:
-        proc.stdin.write((e).encode('utf-8'))
-        proc.stdin.write(struct.pack('B', 0))
-
-    proc.stdin.close()
-    answer = proc.stdout.read().decode("utf-8")
-    exit_code = proc.wait()
-    # trim whitespace
-    if answer == '':
-        return(None, exit_code)
-    else:
-        return(answer.strip('\n'), exit_code)
 
 # Check if program exists - linux only
 def cmd_exists(cmd):
     return subprocess.call("type " + cmd, shell=True, 
         stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
-
-
-
-# Make sure everything is ready for qn
-
-#print("Checking qn directory " + QNDIR + "...")
-if not os.path.isdir(QNDIR):
-    print( "Please create your directory as defined by QNDIR!")
-    print( "As of now, you set QNDIR to be '$QNDIR'")
-    print( "This directory was not found! Exiting...")
-    sys.exit(1)
-if not os.path.exists(QNDATA):
-    os.makedirs(QNDATA, exist_ok=True)
-if not os.path.exists(QNTRASH):
-    os.makedirs(QNTRASH, exist_ok=True)
 
 # Define application launcher
 if cmd_exists('rifle'):
@@ -113,7 +49,6 @@ if sys.stdin.isatty():
 else:
     TERM_INTER=False
     text_editor = QNTERMINAL + ' -e ' + QNEDITOR
-
 
 # outdated option to detect mimetype? Still best it seems
 def file_mime_type(filename):
@@ -203,7 +138,7 @@ def _undelete_note(note):
 #    n.show()
 
 # Opens everything in text...mimetypes pls
-def qnOpenNote(note):
+def qn_open_note(note):
     fulldir = os.path.join(QNDIR, note)
 
     if os.path.isfile(fulldir):
@@ -218,7 +153,7 @@ def qnOpenNote(note):
         sys.exit(1)
 
 
-def qnNewNote(note):
+def qn_new_note(note):
     if '/' in note:
         note_dir = note.rsplit('/',1)[0]
         if not os.path.isdir(note_dir):
@@ -229,7 +164,7 @@ def qnNewNote(note):
 
 
 # In implementation - not yet working
-def qnFindInNotes(file_list, f_string):
+def qn_find_in_notes(file_list, f_string):
     grep_path = os.path.join(QNDIR, '*')
     filt = f_string.strip().split(" ") 
     filtered_list = file_list
@@ -260,147 +195,40 @@ def qnFindInNotes(file_list, f_string):
 
     return(raw_lines, filtered_list, filtered_content)
 
-    
+def check_environment(in_rofi=False):
+    # Make sure everything is ready for qn
 
-
-
-def show_main_rofi(starting_filter=None):
-    HELP  = "\"Enter\" to edit/create, \"" + opt_delete + "\" to delete, "
-    HELP += opt_seetrash + "\" to show trash, "
-    HELP += " \"" + opt_rename + "\" to rename"
-
-    main_files,main_files_full = _list_files(QNDIR)
-    rofi_command = rofi_base_command + ['-mesg', HELP, '-format', 'f;s']
-    if starting_filter:
-        rofi_command += ['-filter', starting_filter]
-
-    SELFS,val = call_rofi(rofi_command,main_files)
-
-    if (SELFS == None):
-        sys.exit(1)
-
-    FILTER,SEL = SELFS.split(';')
-    #print('sel:' + SEL)
-    #print('val:' + str(val))
-
-    if (val == 18):
-        show_delete_rofi(SEL)
-        sys.exit(0)
-    elif (val == 17):
-        show_trash_rofi()
-    elif (val == 16):
-        show_rename_rofi(SEL)
-    elif (val == 15):
-        print('open dir - not yet implemented')
-    elif (val == 14):
-        print('find content - not yet implemented')
-        RESULT = show_filtered_rofi(main_files_full, FILTER)
-        print("Opening " + RESULT + "...")
-        qnOpenNote(RESULT)
-
-    elif (val == 13):
-        if SEL.strip():
-            print("creating note " + FILTER + "...")
-            qnNewNote(FILTER)
-    else:
-        if SEL.strip():
-            path=os.path.join(QNDIR, SEL)
-            if os.path.isfile(path):
-                print("file found, edit...")
-                qnOpenNote(SEL)
+    #print("Checking qn directory " + QNDIR + "...")
+    if not os.path.isdir(QNDIR):
+        HELP_MSG = " Do you want to create the qn directory: " + QNDIR + "?"
+        if in_rofi:
+            if show_yesno_rofi(HELP_MSG):
+                print("Creating directory: " + QNDIR + "...")
+                os.makedirs(QNDIR)
             else:
-                print("file not found, create...")
-                qnNewNote(SEL)
+                print("qn directory" + QNDIR + " does not exist. Exiting...")
+                sys.exit(1)
+        else:
+            s = input(HELP_MSG + " (y/N) ")
+            if s and (s[0] == "Y" or s[0] == "y"):
+                print("Creating directory: " + QNDIR + "...")
+                os.makedirs(QNDIR)
+            else:
+                print("qn directory" + QNDIR + " does not exist. Exiting...")
+                sys.exit(1)
 
-def show_filtered_rofi(mff, FILTER):
-    HELP = "List of notes filtered for '" + FILTER + "'."
-    raw, fnotes, fcont = qnFindInNotes(mff, FILTER.strip())
-    rofi_command = rofi_base_command + ['-p', 'qn search', '-mesg', HELP, 
-            '-columns', '1', '-format', 'i']
-
-    if FILTER == '':
-        show_main_rofi()
-
-    final_list = []
-    n = 0
-    for fn in fnotes:
-        fn_rel = os.path.relpath(fn, QNDIR)
-        if len(fn_rel) > 21:
-            fn_rel = fn_rel[0:10] + '…' + fn_rel[-11:len(fn_rel)]
-
-        fn_rel += "  :  " + fcont[n]
-        final_list.append(fn_rel)
-        n += 1
-
-    SEL,val = call_rofi(rofi_command, final_list)
-    if SEL == None:
-        sys.exit(0)
-    
-    return(fnotes[int(SEL)])
+    if not os.path.exists(QNDATA):
+        print("Creating directory: " + QNDATA + "...")
+        os.makedirs(QNDATA, exist_ok=True)
+    if not os.path.exists(QNTRASH):
+        print("Creating directory: " + QNTRASH + "...")
+        os.makedirs(QNTRASH, exist_ok=True)
 
 
+#def start_qnr ():
+#    in_rofi = True
+#    check_environment(in_rofi)
+#    show_main_rofi()
 
-def show_trash_rofi():
-    HELP = 'Press enter to restore file'
-    trash_files, trash_files_full = _list_files(QNTRASH)
-    rofi_command = rofi_base_command + ['-mesg', HELP]
-    SEL,val = call_rofi(rofi_command, trash_files)
-    if (SEL == None):
-        sys.exit(1)
-    if SEL.strip():
-        show_undelete_rofi(SEL)
-
-def show_yesno_rofi(HELP_MSG):
-    HELP="<span color=\"red\">"
-    HELP+=HELP_MSG
-    HELP+="</span>"
-    rofi_command = rofi_base_command + ['-mesg', HELP, '-columns', '1']
-    SEL,val = call_rofi(rofi_command, ['no','yes'])
-    
-    if SEL:
-        return(SEL == 'yes')
-    else:
-        sys.exit(1)
-
-def show_delete_rofi(note):
-    HELP_MSG = "Are you sure you want to delete \"" + note + "\"?"
-    if show_yesno_rofi(HELP_MSG):
-        print("Deleting " + note)
-        _delete_note(note)
-    else:
-        print("Not Deleting " + note)
-
-def show_undelete_rofi(note):
-    HELP_MSG = "Are you sure you want to restore \"" + note + "\"?"
-    if show_yesno_rofi(HELP_MSG):
-        print("Restoring " + note)
-        _undelete_note(note)
-    else:
-        print("Not Restoring " + note)
-
-def show_rename_rofi(note):
-    HELP = "Please write the new name for this file"
-    rofi_command = rofi_base_command + ['-mesg', HELP, '-columns', '1',
-                    '-p', 'qn', 'rename', '-filter', note]
-
-    SEL, val = call_rofi(rofi_command, [''])
-
-    if (SEL == None):
-        sys.exit(1)
-
-    _move_note(note.strip(), SEL.strip())
-
-
-
-
-def start_qn ():
-
-    show_main_rofi()
-
-start_qn()
-timeb = time.time()
-print('delta t: ' + str(timeb-timea))
-#print(_delete_note('del/file1'))
-#print(_undelete_note('del/file1'))
-#print(show_yesno_rofi('are you sure you want to delete this?'))
-#show_undelete_rofi('del/file1')
+#start_qnr()
+#timeb = time.time()
---
+++ qnr.py
@@ -0,0 +1,185 @@
+from subprocess import Popen,PIPE,call
+import qn
+import os
+import sys
+import struct
+
+COLS=3
+
+# delete note
+opt_delete = 'Alt+Backspace'
+# see trashed notes with the ability to restore them
+opt_seetrash = 'Alt+t'
+# rename note
+opt_rename = 'Alt+Space'
+# Open selected note's directory (not yet implemented)
+opt_open_dir = 'Alt+d'
+# Grep notes (not yet implemented)
+opt_filter_content = 'Alt+s'
+# Force create a note
+opt_force_new = 'Alt+Return'
+
+
+rofi_base_command = ['rofi', '-dmenu', '-p', 'qn:']
+
+# code borrowed from
+# https://github.com/DaveDavenport/Rofication/blob/master/rofication-gui.py
+def call_rofi(rofi_command, entries, additional_args=[]):
+    additional_args.extend([ '-kb-custom-9', opt_delete,
+                             '-kb-custom-8', opt_seetrash,
+                             '-kb-custom-7', opt_rename,
+                             '-kb-custom-6', opt_open_dir,
+                             '-kb-custom-5', opt_filter_content,
+                             '-kb-custom-4', opt_force_new,
+                             '-markup-rows',
+                             '-sep', '\\0',
+#                             '-format', 'i',
+                             '-columns', str(COLS),
+#                             '-lines', '4',
+#                             '-eh', '2',
+#                             '-location', '2', '-width', '100' ])
+                            ])
+
+    proc = Popen(rofi_command + additional_args, stdin=PIPE, stdout=PIPE)
+
+    for e in entries:
+        proc.stdin.write((e).encode('utf-8'))
+        proc.stdin.write(struct.pack('B', 0))
+
+    proc.stdin.close()
+    answer = proc.stdout.read().decode("utf-8")
+    exit_code = proc.wait()
+    # trim whitespace
+    if answer == '':
+        return(None, exit_code)
+    else:
+        return(answer.strip('\n'), exit_code)
+
+
+def show_main_rofi(starting_filter=None):
+    HELP  = "\"Enter\" to edit/create, \"" + opt_delete + "\" to delete, "
+    HELP += opt_seetrash + "\" to show trash, "
+    HELP += " \"" + opt_rename + "\" to rename"
+
+    main_files,main_files_full = qn._list_files(qn.QNDIR)
+    rofi_command = rofi_base_command + ['-mesg', HELP, '-format', 'f;s']
+    if starting_filter:
+        rofi_command += ['-filter', starting_filter]
+
+    SELFS,val = call_rofi(rofi_command,main_files)
+
+    if (SELFS == None):
+        sys.exit(1)
+
+    FILTER,SEL = SELFS.split(';')
+    #print('sel:' + SEL)
+    #print('val:' + str(val))
+
+    if (val == 18):
+        show_delete_rofi(SEL)
+        sys.exit(0)
+    elif (val == 17):
+        show_trash_rofi()
+    elif (val == 16):
+        show_rename_rofi(SEL)
+    elif (val == 15):
+        print('open dir - not yet implemented')
+    elif (val == 14):
+        print('find content - not yet implemented')
+        RESULT = show_filtered_rofi(main_files_full, FILTER)
+        print("Opening " + RESULT + "...")
+        qn_open_note(RESULT)
+
+    elif (val == 13):
+        if SEL.strip():
+            print("creating note " + FILTER + "...")
+            qn.qn_new_note(FILTER)
+    else:
+        if SEL.strip():
+            path=os.path.join(qn.QNDIR, SEL)
+            if os.path.isfile(path):
+                print("file found, edit...")
+                qn_open_note(SEL)
+            else:
+                print("file not found, create...")
+                qn.qn_new_note(SEL)
+
+def show_filtered_rofi(mff, FILTER):
+    HELP = "List of notes filtered for '" + FILTER + "'."
+    raw, fnotes, fcont = qn_find_in_notes(mff, FILTER.strip())
+    rofi_command = rofi_base_command + ['-p', 'qn search', '-mesg', HELP, 
+            '-columns', '1', '-format', 'i']
+
+    if FILTER == '':
+        show_main_rofi()
+
+    final_list = []
+    n = 0
+    for fn in fnotes:
+        fn_rel = os.path.relpath(fn, qn.QNDIR)
+        if len(fn_rel) > 21:
+            fn_rel = fn_rel[0:10] + '…' + fn_rel[-11:len(fn_rel)]
+
+        fn_rel += "  :  " + fcont[n]
+        final_list.append(fn_rel)
+        n += 1
+
+    SEL,val = call_rofi(rofi_command, final_list)
+    if SEL == None:
+        sys.exit(0)
+    
+    return(fnotes[int(SEL)])
+
+def show_trash_rofi():
+    HELP = 'Press enter to restore file'
+    trash_files, trash_files_full = qn._list_files(qn.QNTRASH)
+    rofi_command = rofi_base_command + ['-mesg', HELP]
+    SEL,val = call_rofi(rofi_command, trash_files)
+    if (SEL == None):
+        sys.exit(1)
+    if SEL.strip():
+        show_undelete_rofi(SEL)
+
+def show_yesno_rofi(HELP_MSG):
+    HELP="<span color=\"red\">"
+    HELP+=HELP_MSG
+    HELP+="</span>"
+    rofi_command = rofi_base_command + ['-mesg', HELP, '-columns', '1']
+    SEL,val = call_rofi(rofi_command, ['no','yes'])
+    
+    if SEL:
+        return(SEL == 'yes')
+    else:
+        sys.exit(1)
+
+def show_delete_rofi(note):
+    HELP_MSG = "Are you sure you want to delete \"" + note + "\"?"
+    if show_yesno_rofi(HELP_MSG):
+        print("Deleting " + note)
+        qn._delete_note(note)
+    else:
+        print("Not Deleting " + note)
+
+def show_undelete_rofi(note):
+    HELP_MSG = "Are you sure you want to restore \"" + note + "\"?"
+    if show_yesno_rofi(HELP_MSG):
+        print("Restoring " + note)
+        qn._undelete_note(note)
+    else:
+        print("Not Restoring " + note)
+
+def show_rename_rofi(note):
+    HELP = "Please write the new name for this file"
+    rofi_command = rofi_base_command + ['-mesg', HELP, '-columns', '1',
+                    '-p', 'qn', 'rename', '-filter', note]
+
+    SEL, val = call_rofi(rofi_command, [''])
+
+    if (SEL == None):
+        sys.exit(1)
+
+    qn._move_note(note.strip(), SEL.strip())
+
+if __name__ == '__main__':
+    qn.check_environment(True)
+    show_main_rofi()
