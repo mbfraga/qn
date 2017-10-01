b6cdad22515c3c13cd5adc04968ab8449262b037
Martin Fraga <mbfraga@gmail.com>
2017-10-01 03:39:23 -0700

    setup.py can handle non-root installs
    If the installation is run as root, the /etc/qn/config file is created.
    Otherwise it is not. This prevents issues in systems like termux.

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from os import geteuid
+
+data_files = []
+if geteuid() == 0:
+    data_files.append(('/etc/qn', ['config.example']))
 
 setup(name='qn',
-    version='0.1',
-    description='Quick note manager with fzf/rofi interfaces.',
-    author='mbfraga',
-    author_email='mbfraga@gmail.com',
-    url='https://www.github.com/mbfraga/qn/',
-    packages=['qn'],
-    scripts=['bin/qnr', 'bin/qnf'],
-    data_files=[('/etc/qn', ['config.example'])],
-    install_requires=['configargparse'],
-    )
+      version='0.1',
+      description='Quick note manager with fzf/rofi interfaces.',
+      author='mbfraga',
+      author_email='mbfraga@gmail.com',
+      url='https://www.github.com/mbfraga/qn/',
+      packages=['qn'],
+      scripts=['bin/qnr', 'bin/qnf'],
+      data_files=data_files,
+      install_requires=['configargparse'],
+      )
